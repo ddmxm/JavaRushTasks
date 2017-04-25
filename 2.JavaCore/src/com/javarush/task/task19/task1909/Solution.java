5cd13652ee3e6c90e6c512bafdf298af303a0c96
ddmxm <korachev@gmail.com>
2017-04-25 17:22:50 +0300

    task19 выполнено задание 1909.

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1909/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task19.task1909;
+
+/* 
+Замена знаков
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+             BufferedReader fileReader = new BufferedReader(new FileReader(bufferedReader.readLine()));
+             BufferedWriter fileWriter = new BufferedWriter(new FileWriter(bufferedReader.readLine()));
+        ) {
+            String word = "";
+            while (fileReader.ready()) {
+                char c = (char) fileReader.read();
+                if (c == '.') {
+                    fileWriter.write((int) '!');
+                } else {
+                    fileWriter.write((int) c);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
