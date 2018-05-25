527d9ef61d47bafa277853578f58e358be9f78dd
ddmxm <korachev@gmail.com>
2018-05-25 15:15:25 +0300

    task21 выполнено задание 2104.

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2104/Solution.java
@@ -0,0 +1,49 @@
+package com.javarush.task.task21.task2104;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+Equals and HashCode
+*/
+public class Solution {
+    private final String first, last;
+
+    public Solution(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null)
+            return false;
+
+        if (!(o instanceof Solution)) {
+            return false;
+        }
+
+        if (this == o) return true;
+
+        Solution solution = (Solution) o;
+
+        if (first != null ? !first.equals(solution.first) : solution.first != null) return false;
+        if (last != null ? !last.equals(solution.last) : solution.last != null) return false;
+
+        return true;
+    }
+
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((first == null) ? 0 : first.hashCode()) +
+                ((last == null) ? 0 : last.hashCode());
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Set<Solution> s = new HashSet<>();
+        s.add(new Solution("Donald", "Duck"));
+        System.out.println(s.contains(new Solution("Donald", "Duck")));
+    }
+}
