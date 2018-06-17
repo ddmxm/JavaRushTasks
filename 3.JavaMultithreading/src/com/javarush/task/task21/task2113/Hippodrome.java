7f2174d9e00513dd36ede87ee9e2a7790a76b5e9
ddmxm <korachev@gmail.com>
2018-06-17 12:07:22 +0300

    task21 выполнено задание 2113.

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -1,9 +1,11 @@
 package com.javarush.task.task21.task2113;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Hippodrome {
-    private List<Horse> horses;
+    static Hippodrome game;
+    private static List<Horse> horses = null;
 
     public List<Horse> getHorses() {
         return horses;
@@ -14,6 +16,9 @@ public class Hippodrome {
     }
 
     public static void main(String[] args) {
-
+        game = new Hippodrome(new ArrayList<>());
+        horses.add(new Horse("Leela", 3, 0));
+        horses.add(new Horse("Amy", 3, 0));
+        horses.add(new Horse("Tress", 3, 0));
     }
 }
